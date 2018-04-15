b8a2c0e3ea8aa2e85b08d3ff45f836fd26070e5a
Alex Binns <ae8698ae@ALEX-DESKTOP>
2018-04-15 15:18:21 +1200

    Tutorial 4 completed.

--- AlexBinns07185952Tutorial4.py
+++ AlexBinns07185952Tutorial4.py
@@ -1,6 +1,7 @@
 import random
 
-def randomised_st_connectivity(file_name):
+
+def read_file_traverse_graph(file_name):
     with open(file_name) as f:
         number_of_nodes = int(f.readline().strip())
         start_node = int(f.readline().strip())
@@ -8,32 +9,42 @@ def randomised_st_connectivity(file_name):
         f.readline()
         graph = [line.strip().split(' ') for line in f if line != '\n']
 
-        for i in range(len(graph)):
-            graph[i] = list(map(int, graph[i]))
+        for index in range(len(graph)):
+            graph[index] = list(map(int, graph[index]))
 
     count = graph_traversal(start_node, end_node, number_of_nodes, graph)
-
     return count
 
 
-def graph_traversal(start_vertice, end_vertice, number_of_nodes, graph):
-
+def graph_traversal(start_node, end_node, number_of_nodes, graph):
     step_count = 0
-    current_index = start_vertice
+    current_index = start_node
 
-    while (current_index != end_vertice) and (step_count < 2 * number_of_nodes ** 3):
+    while (current_index != end_node) and (step_count < 2 * number_of_nodes ** 3):
         possible_vertices = []
         for iterator in range(number_of_nodes - 1):
             if graph[current_index][iterator] == 1:
                 possible_vertices.append(iterator)
 
         current_index = random.choice(possible_vertices)
-        print(current_index)
         step_count += 1
-    if current_index == end_vertice:
-        return(True, step_count)
+
+    if current_index == end_node:
+        return True, step_count
     else:
-        return (False, step_count)
+        return False, step_count
 
 
-print(randomised_st_connectivity("testgraph7.txt"))
+still_more = True
+
+while still_more:
+    input_file = input("Please input filename ")
+    try:
+        result = read_file_traverse_graph(input_file)
+        print("For the file {} the traversal returned {} and the number of steps were {}"
+              .format(input_file, result[0], result[1]))
+    except FileNotFoundError:
+        print("That is not a valid filename please try again")
+    user_continue = str.lower(input("Do you have another file? (y/n) "))
+    if user_continue == 'n':
+        still_more = False
